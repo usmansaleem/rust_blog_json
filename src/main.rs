9a51fe476b38ae44f8e950279228189ca89a596f
Usman Saleem <usman@usmans.info>
2020-11-09 17:24:18 +1000

    moving logic from main to lib
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,5 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-
-use anyhow::{Context, Result};
-use chrono::prelude::*;
-use serde::Deserialize;
-use serde::Serialize;
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Blog {
-    blog_entries: Vec<BlogEntry>,
-    next_id: u32,
-}
-
-impl Blog {
-    fn read_from_file<P: AsRef<Path>>(path: P) -> Result<Blog> {
-        // open file in read-only mode
-        let file = File::open(path.as_ref())
-            .with_context(|| format!("Unable to read file {}", path.as_ref().display()))?;
-        let reader = BufReader::new(file);
-
-        // read json from file as vector
-        let blog: Blog = serde_json::from_reader(reader)
-            .with_context(|| format!("Error parsing as JSON {}", path.as_ref().display()))?;
-
-        Ok(blog)
-    }
-
-    fn new_blog_entry(
-        &mut self,
-        url_friendly_id: String,
-        title: String,
-        description: String,
-        body: String,
-        categories: Vec<BlogCategory>,
-    ) {
-        let blog_entry = BlogEntry::new(
-            self.next_id,
-            url_friendly_id,
-            title,
-            description,
-            body,
-            categories,
-        );
-        self.next_id += 1;
-
-        // insert into vec (ownership transferred)
-        self.blog_entries.insert(0, blog_entry);
-    }
-
-    fn delete_blog_entry(&mut self, id: u32) {
-        self.blog_entries.retain(|entry| entry.id != id);
-    }
-
-    fn find_by_url(&self, url_friendly_id: &'static str) -> Option<&BlogEntry> {
-        self.blog_entries
-            .iter()
-            .find(|entry| entry.url_friendly_id == *url_friendly_id)
-    }
-
-    // TODO: Save blog back to file
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct BlogEntry {
-    id: u32,
-    url_friendly_id: String,
-    title: String,
-    description: String,
-    body: String,
-    blog_section: String,
-    created_on: String,
-    modified_on: String,
-    categories: Vec<BlogCategory>,
-}
-
-impl BlogEntry {
-    fn new(
-        id: u32,
-        url_friendly_id: String,
-        title: String,
-        description: String,
-        body: String,
-        categories: Vec<BlogCategory>,
-    ) -> BlogEntry {
-        BlogEntry {
-            id,
-            url_friendly_id,
-            title,
-            description,
-            body,
-            blog_section: "Main".to_string(),
-            created_on: Utc::now().format("%Y-%m-%d").to_string(),
-            modified_on: Utc::now().format("%Y-%m-%d").to_string(),
-            categories,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct BlogCategory {
-    name: String,
-}
-
-impl BlogCategory {
-    fn new(name: String) -> BlogCategory {
-        BlogCategory { name }
-    }
-}
+use anyhow::Result;
+use blog_json::{Blog, BlogCategory};
 
 fn main() -> Result<()> {
     println!("Reading Blog data from data.json");
@@ -125,7 +14,6 @@ fn main() -> Result<()> {
     } else {
         println!("Unexpected behavior - wrapper_over_runas_utility not found");
     }
-
 
     // insert a new blog entry in blog
     println!("Inserting new Blog Entry");
